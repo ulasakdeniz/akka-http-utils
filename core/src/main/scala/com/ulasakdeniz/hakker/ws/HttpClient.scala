bfe0c456cd2c0821e8bbd03abc9b4b4242366ed6
ulasakdeniz <akdenizulas@gmail.com>
2016-08-31 23:42:25 +0300

    initial ws

---
+++ core/src/main/scala/com/ulasakdeniz/hakker/ws/HttpClient.scala
@@ -0,0 +1,18 @@
+package com.ulasakdeniz.hakker.ws
+
+import akka.http.scaladsl.model.{HttpMethod, HttpMethods, HttpRequest, HttpResponse}
+import com.ulasakdeniz.hakker.System
+
+import scala.concurrent.Future
+
+object HttpClient extends System {
+
+  def makeRequest(uri: String, method: HttpMethod = HttpMethods.GET): Future[HttpResponse] = {
+    val request = HttpRequest(method, uri)
+    makeRequest(request)
+  }
+
+  def makeRequest(request: HttpRequest): Future[HttpResponse] = {
+    http.singleRequest(request)
+  }
+}
