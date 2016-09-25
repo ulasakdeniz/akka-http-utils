664555423a4dfe87b88f0dde9fd646b66fbd8159
ulasakdeniz <akdenizulas@gmail.com>
2016-09-25 21:40:08 +0300

    http client test

--- ws/src/test/scala/com/ulasakdeniz/hakker/ws/http/HttpClientUnitSpec.scala
+++ ws/src/test/scala/com/ulasakdeniz/hakker/ws/http/HttpClientUnitSpec.scala
@@ -1,22 +1,51 @@
 package com.ulasakdeniz.hakker.ws.http
 
-import akka.http.scaladsl.model.{HttpEntity, HttpResponse, StatusCodes}
+import akka.http.scaladsl.model._
 import com.ulasakdeniz.hakker.base.UnitSpec
-import io.circe.Json
+import org.scalatest.Assertions
 
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Future, TimeoutException}
 
-class HttpClientUnitSpec extends UnitSpec {
+class HttpClientUnitSpec extends UnitSpec with Assertions {
 
   import com.ulasakdeniz.hakker.System._
 
-  "makeRequest" should {
+  "execute" should {
 
-    "create an HttpRequest with parameters and return a ResponseContainer" in new HttpClientUnitSpecFixture {
-      val request      = ???
-      val response = TestHttpClient.execute(request)
-      val json = response.entityAs[Json]
-      json.futureValue.toString shouldEqual ""
+    "create an HttpResponse from the given HttpRequest" in new HttpClientUnitSpecFixture {
+      val request  = HttpRequest()
+      val response = HttpResponse()
+      when(spiedHttp.singleRequest(request)(mat)).thenReturn(Future.successful(response))
+
+      val result = TestHttpClient.execute(request)
+      result.futureValue shouldEqual response
+    }
+
+    "create an HttpResponse restricted with a timeout" in new HttpClientUnitSpecFixture {
+      val request  = HttpRequest()
+      val response = HttpResponse()
+      when(spiedHttp.singleRequest(request)(mat)).thenReturn(Future.successful(response))
+
+      val result = TestHttpClient.execute(request, 3.seconds)
+      result.futureValue shouldEqual response
+    }
+
+    "fail if timeout exceeds" in new HttpClientUnitSpecFixture {
+      val request  = HttpRequest()
+      val response = HttpResponse()
+      val future = Future {
+        val exceededTime = 40000
+        Thread.sleep(exceededTime)
+        response
+      }
+      when(spiedHttp.singleRequest(request)(mat)).thenReturn(future)
+
+      val result = TestHttpClient.execute(request, 3.seconds)
+      result.onFailure {
+        case t: TimeoutException => succeed
+      }
     }
   }
 
@@ -24,7 +53,7 @@ class HttpClientUnitSpec extends UnitSpec {
     val spiedHttp = spy(http)
 
     object TestHttpClient extends AbstractHttpClient {
-      override lazy val http                            = spiedHttp
+      override lazy val http = spiedHttp
     }
   }
 }
